c68acbb61f01b23cdf19c9ad92180475647553d6
eth-button <eth-button@users.noreply.github.com>
2018-02-12 01:52:38 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+import {
+  bufferToHex,
+  generateAddress,
+  isValidPrivate,
+  privateToPublic,
+  publicToAddress,
+  sha3,
+  toChecksumAddress,
+  toBuffer
+} from "ethereumjs-util";
+import { encryptPrivateKey, generatePrivateKey } from "./lib/crypto";
+import { v4 } from "uuid";
+
+export default class Address {
+  constructor(privateKey) {
+    let _privateKey;
+    if (privateKey !== undefined) {
+      _privateKey = toBuffer(privateKey);
+      if (!isValidPrivate(_privateKey)) {
+        throw new Error(privateKey + " is not a valid private key");
+      }
+    } else {
+      _privateKey = generatePrivateKey();
+    }
+    this._privateKey = _privateKey;
+    this._publicKey = privateToPublic(this._privateKey);
+    this._address = publicToAddress(this._publicKey);
+    this.privateKey = bufferToHex(this._privateKey);
+    this.publicKey = bufferToHex(this._publicKey);
+    this.rawAddress = bufferToHex(this._address);
+    this.address = toChecksumAddress(this.rawAddress);
+  }
+
+  getContractAddress(nonce) {
+    const address = generateAddress(this._address, nonce);
+    return bufferToHex(address);
+  }
+
+  generateWallet(password) {
+    const privateKey = this._privateKey;
+    const address = this.address;
+    const crypto = encryptPrivateKey(privateKey, password);
+    return {
+      crypto,
+      id: v4(),
+      version: 3
+    };
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+var fs = require("fs");
+
+var package = fs.readFileSync("package.json", "utf-8");
+package = JSON.parse(package);
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "dist/" + package.name + ".js",
+    library: "Address"
+  },
+  plugins: [new UglifyJSPlugin()]
+};
